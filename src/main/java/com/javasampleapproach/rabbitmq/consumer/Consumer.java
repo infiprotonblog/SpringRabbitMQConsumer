80574f9d5053f333b05b1ae55951c073ab79f471
Divya <divya@infiproton.com>
2020-11-06 20:00:13 +0530

    consumer

---
+++ src/main/java/com/javasampleapproach/rabbitmq/consumer/Consumer.java
@@ -0,0 +1,14 @@
+package com.javasampleapproach.rabbitmq.consumer;
+
+import com.javasampleapproach.rabbitmq.model.Company;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+	
+	@RabbitListener(queues="${jsa.rabbitmq.queue}", containerFactory="jsaFactory")
+    public void recievedMessage(Company company) {
+        System.out.println("Recieved Message: " + company);
+    }
+}
